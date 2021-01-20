3a4bbda6fd216a3c22af2a5168150e7fde42053a
khan567 <muhammad.khalil29@yahoo.com>
2021-01-20 21:01:39 +0100

    adding files

--- src/test/java/StepDefinition/HomeItems.java
+++ src/test/java/StepDefinition/HomeItems.java
@@ -4,26 +4,13 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
 
 public class HomeItems {
-    @Then("User sees the Home page")
-    public void userSeesTheHomePage() {
-        
-    }
-
-    @When("User search for {string}")
-    public void userSearchFor(String arg0) {
-        
-    }
-
-    @Then("User sees the Donuts")
-    public void userSeesTheDonuts() {
-        
-    }
+    WebDriver driver;
 
     @When("User add {string} to the order")
     public void userAddToTheOrder(String arg0) {
-        
     }
 
     @Then("User sees order created successfully")
@@ -31,92 +18,97 @@ public class HomeItems {
 
     }
 
-    @When("User search for second order of {string}")
-    public void userSearchForSecondOrderOf(String arg0) {
-        
+    @When("User try to create second order of {string}")
+    public void userTryToCreateSecondOrderOf(String arg0) {
+
     }
 
     @Then("User sees an error message {string}")
     public void userSeesAnErrorMessage(String arg0) {
-        
+
     }
 
     @When("User try to edit the above created order")
     public void userTryToEditTheAboveCreatedOrder() {
-        
+
     }
 
-    @Then("User sees the existing order for {string}")
-    public void userSeesTheExistingOrderFor(String arg0) {
-        
+    @And("User has already existed order")
+    public void userHasAlreadyExistedOrder() {
+
     }
 
     @When("User cancel the existing order for {string}")
     public void userCancelTheExistingOrderFor(String arg0) {
-        
+
     }
 
     @Then("User sees {string} message")
     public void userSeesMessage(String arg0) {
-        
+
     }
 
-    @Then("User search for donuts")
-    public void userSearchForDonuts() {
-        
-    }
+    @When("User place an order with {string} donuts")
+    public void userPlaceAnOrderWithDonuts(String arg0) {
 
-    @When("User add donuts to the order")
-    public void userAddDonutsToTheOrder() {
-        
-    }
-
-    @Then("User sees order {string} created successfully")
-    public void userSeesOrderCreatedSuccessfully(String arg0) {
-        
     }
 
     @Then("User sees order {string} created successfully for client ID {string}")
     public void userSeesOrderCreatedSuccessfullyForClientID(String arg0, String arg1) {
-        
+
     }
 
-    @And("User sees orders are sorted by the number of seconds")
-    public void userSeesOrdersAreSortedByTheNumberOfSeconds() {
-        
+    @Given("User is a regular customer with client ID {string}")
+    public void userIsARegularCustomerWithClientID(String arg0) {
+
     }
 
-    @Given("User login with premium customer ID {string}")
-    public void userLoginWithPremiumCustomerID(String arg0) {
-        
+    @Then("User sees order {string} created successfully for client ID {string}")
+    public void userSeeOrderCreatedSuccessfullyForClientID(String arg0, String arg1) {
+
     }
 
-    @And("User sees order from premium customer is given higher priority")
-    public void userSeesOrderFromPremiumCustomerIsGivenHigherPriority() {
-        
+    @And("User sees order are sorted by the number of seconds")
+    public void userSeesOrderAreSortedByTheNumberOfSeconds() {
+
     }
 
-    @Then("User sees the order position in the queue along with wait time")
-    public void userSeesTheOrderPositionInTheQueueAlongWithWaitTime() {
-        
+    @Given("User is a premium customer with client ID {string}")
+    public void userIsAPremiumCustomerWithClientID(String arg0) {
+
+    }
+
+    @And("User sees order from premium customers is given higher priority")
+    public void userSeesOrderFromPremiumCustomersIsGivenHigherPriority() {
+
+    }
+
+    @Given("User is a {string} client having order in the queue")
+    public void userIsAClientHavingOrderInTheQueue(String arg0) {
+
+    }
+
+    @Then("User sees my order position in the queue along with wait time")
+    public void userSeesMyOrderPositionInTheQueueAlongWithWaitTime() {
+
     }
 
     @And("Wait time should change based on orders in the queue")
     public void waitTimeShouldChangeBasedOnOrdersInTheQueue() {
-        
+
+    }
+
+    @Given("There are multiple order in the queue")
+    public void thereAreMultipleOrderInTheQueue() {
+
     }
 
     @When("Manager check for entries in the queue")
     public void managerCheckForEntriesInTheQueue() {
-        
+
     }
 
-    @And("There are multiple order in the queue")
-    public void thereAreMultipleOrderInTheQueue() {
-        
-    }
-
-    @Then("Manager sees all the entries in the queue are listed along with wait time")
-    public void managerSeesAllTheEntriesInTheQueueAreListedAlongWithWaitTime() {
+    @Then("All the entries in the queue are listed along with wait time")
+    public void allTheEntriesInTheQueueAreListedAlongWithWaitTime() {
     }
 }
--- src/test/java/StepDefinition/Login.java
+++ src/test/java/StepDefinition/Login.java
@@ -1,33 +1,14 @@
 package StepDefinition;
 
-import Common.Base;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import org.apache.tools.ant.taskdefs.WaitFor;
-import org.openqa.selenium.WebDriver;
-
 
 public class Login {
 
-    @Given("User login with{string}")
-    public void userLoginWith(String arg0) {
-
-    }
-    @And("User logout")
-    public void userLogout() {
+    @Given("User login with {string} belongs to {string}")
+    public void userLoginWithBelongsTo(String arg0, String arg1) {
     }
 
-    @Given("User login with regular customer ID {string}")
-    public void userLoginWithRegularCustomerID(String arg0) {
-        
-    }
-
-    @Given("Manager login with ID")
-    public void managerLoginWithID() {
-        
-    }
-
-    @And("Manager logout")
-    public void managerLogout() {
+    @Given("User is a regular customer login with client ID {string}")
+    public void userIsARegularCustomerLoginWithClientID(String arg0) {
     }
 }
